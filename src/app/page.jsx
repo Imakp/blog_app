88589e489e59bb22a2d7ff9e39547d4074a76782
Imakp <theankitkumarpandit@gmail.com>
2024-01-26 19:39:22 +0530

    minnor updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import styles from "./homepage.module.css";
 import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
-import Menu from "@/components/Menu/Menu";
+import Menu from "@/components/Menubar/Menu";
 
 export default function Home({ searchParams }) {
   const page = parseInt(searchParams.page) || 1;
